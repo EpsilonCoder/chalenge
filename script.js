1967a0ea9211b88794f4b18bb0d2aa96df3bdb8e
hacker1998-thosiba <sowcheikh086@gmail.com>
2021-11-08 18:55:08 +0000

    Optimisation

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
          var buttoncompte=document.getElementById('buttoncompte');
          var recompt=document.getElementById('recompt');
+         var ok=document.getElementById('francaisdate');
          buttoncompte.addEventListener('click',zicre);
          recompt.addEventListener('click',recommencer);
+         var music = document.getElementById("music")
 
              	
          var i=1;
      	
      	function zicre() {
+            music.load();
+            music.play()
+           
      		var x=document.getElementById('compteur');
 
      		   x.innerHTML=i;
@@ -18,3 +23,22 @@
      		x.innerHTML=0;
                i=1;
          }
+
+
+
+ function printTime() {
+    var d = new Date();
+    var fullYear = d.getFullYear();
+    var month = d.getMonth();
+    var date = d.getDate();
+    var day = d.getDay();
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var secs = d.getSeconds();
+    ok.innerHTML ="Le "+date+ "/ "+month+ "/" +fullYear+"<br>"+hours+":"+mins+":"+secs;
+   // "Year"+fullYear+".Month"+month+".Le "+date+".Day"+day+".<br>"+hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
+  document.write("<br>");
+ 
+
